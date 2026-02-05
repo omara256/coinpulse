14dfecafc1c85702340dedb2bd97e9a99f9055a7
omara augustine <omaraperker56@gmail.com>
2026-02-05 10:09:54 +0300

    Implement Coin Overview and Trending Coins Ui and Functionality using the fetcher uitility function

---
+++ components/home/fallback.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import DataTable from '@/components/DataTable';
+
+export const CoinOverviewFallback = () => {
+  return (
+    <div id="coin-overview-fallback">
+      <div className="header pt-2">
+        <div className="header-image bg-dark-400 animate-pulse" />
+        <div className="info">
+          <div className="header-line-sm bg-dark-400 animate-pulse" />
+          <div className="header-line-lg bg-dark-400 animate-pulse" />
+        </div>
+      </div>
+
+      <div className="flex gap-2 md:gap-3 mt-4">
+        {[1, 2, 3, 4, 5, 6].map((i) => (
+          <div key={i} className="period-button-skeleton bg-dark-400 animate-pulse" />
+        ))}
+      </div>
+
+      <div className="chart mt-4">
+        <div className="chart-skeleton bg-dark-400 animate-pulse" />
+      </div>
+    </div>
+  );
+};
+
+export const TrendingCoinsFallback = () => {
+  const columns: DataTableColumn<any>[] = [
+    {
+      header: 'Name',
+      cellClassName: 'name-cell',
+      cell: () => (
+        <div className="name-link pl-0!">
+          <div className="name-image bg-dark-400 animate-pulse" />
+          <div className="name-line bg-dark-400 animate-pulse" />
+        </div>
+      ),
+    },
+    {
+      header: '24h Change',
+      cellClassName: 'change-cell',
+      cell: () => (
+        <div className="price-change">
+          <div className="change-icon bg-dark-400 animate-pulse" />
+          <div className="change-line bg-dark-400 animate-pulse" />
+        </div>
+      ),
+    },
+    {
+      header: 'Price',
+      cellClassName: 'price-cell',
+      cell: () => <div className="price-line bg-dark-400 animate-pulse" />,
+    },
+  ];
+
+  const dummyData = Array(6).fill({});
+
+  return (
+    <div id="trending-coins-fallback">
+      <h4>Trending Coins</h4>
+      <div className="trending-coins-table">
+        <DataTable
+          data={dummyData}
+          columns={columns}
+          rowKey={(_, i) => i}
+          tableClassName="trending-coins-table"
+          headerRowClassName="bg-dark-500!"
+        />
+      </div>
+    </div>
+  );
+};
